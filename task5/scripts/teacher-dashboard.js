519da37e51ac15f5527e5bd8149e4e5bbd57ac04
Rushikesh-Shelar-Zeus <rushikesh.shelar@zeuslearning.com>
2025-07-08 18:58:01 +0530

    Task 5

---
+++ task5/scripts/teacher-dashboard.js
@@ -0,0 +1,27 @@
+function handleMobileMenu() {
+    const header = document.querySelector('header');
+    const nav = document.querySelector('nav');
+    const items = document.querySelectorAll(".item");
+
+    header.classList.toggle('mobile');
+    items.forEach(item => item.classList.toggle('mobile'))
+
+    nav.classList.toggle('mobile')
+
+}
+
+
+const links = document.querySelectorAll('.item');
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        links.forEach(l => l.classList.remove('active'));
+        link.classList.add('active');
+    });
+});
+
+// window.addEventListener('resize', () => {
+//     if (window.innerWidth > 768) {
+//         handleMobileMenu()
+//     }
+// });
